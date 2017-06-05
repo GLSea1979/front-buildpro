fc0f48ef9d20d002635bb1964123adae99e69c69
GLSea1979 <builtbygl@gmail.com>
2017-06-05 13:15:08 -0700

    finished server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-// app.use(express.static(`${__dirname/build}`));
+app.use(express.static(`${__dirname/build}`));
 
 app.listen(PORT, () => {
   console.log('server up', PORT);
